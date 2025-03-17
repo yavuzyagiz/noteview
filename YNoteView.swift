fc1823e54a9a82337d62f4e0b1972b20a531af17
Yavuz <61160862+yavuzyagiz@users.noreply.github.com>
2025-03-17 13:15:10 +0300

    add NoteView file

---
+++ YNoteView.swift
@@ -0,0 +1,50 @@
+//
+//  YNoteView.swift
+//  YNoteView
+//
+//  Created by YZ on 2.01.2025.
+//
+
+import SwiftUI
+
+struct YNoteView: View {
+    @State private var showSheet = false
+    @State private var viewModel: DrawingViewModel = DrawingViewModel()
+    var menuBackgroundColor: Color = .black
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            ToolMenu(viewModel: viewModel, backgroundColor: menuBackgroundColor)
+            ScrollViewReader { ScrollViewProxy in
+                ScrollView {
+                    ZStack {
+                        Canvas { context, size in
+                            viewModel.update(context: context)
+                        }
+                        .gesture(
+                            DragGesture(minimumDistance: 0)
+                                .onChanged { value in
+                                    viewModel.continueDrawing(point: value.location)
+                                }
+                                .onEnded { _ in
+                                    viewModel.endDrawing()
+                                }
+                        )
+                        
+                        TextEditor(text: $viewModel.text)
+                            .customTextEditorStyle()
+                            .disabled(viewModel.selectedTool != nil)
+                            .id("textEditor")
+                    }
+                    .frame(minHeight: 800)
+                }
+            }
+        }
+        .frame(maxHeight: .infinity)
+        .background(.white)
+    }
+}
+
+#Preview {
+    YNoteView()
+}
